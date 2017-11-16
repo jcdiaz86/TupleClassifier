3c1dc03d4d7ccdfed1e3dcb05f9546d88040b01c
Juan Diaz <jcdiaz86@outlook.com>
2017-11-15 19:54:27 -0500

    Testing

--- src/Set.java
+++ src/Set.java
@@ -8,15 +8,25 @@ public class Set {
 
     private ArrayList<ArrayList<String>> table;
 
+    /**
+     * Creates an empty set
+     */
     public Set() {
         table = new ArrayList<ArrayList<String>>();
     }
 
-    public void addTuple(ArrayList<String> tuple)
-    {
+    /**
+     * Adds a tuple to the set
+     * @param tuple to be added
+     */
+    public void addTuple(ArrayList<String> tuple) {
         table.add(tuple);
     }
 
+    /**
+     * Creates a copy of a set leaving the original
+     * @return copy of set
+     */
     public Set getCopy(){
         Set copy = new Set();
         for (ArrayList<String> current : table){
@@ -25,8 +35,11 @@ public class Set {
         return copy;
     }
 
-    //gets the most frequent class label
-    //assumes class label is in the last column
+    /**
+     * Returns the most frequent tuple labels in the set
+     * Assumes the labels to always be on the last column
+     * @return A String of the most frequent label
+     */
     public String getClassLabelFrequent()
     {
         int classIndex = table.get(0).size() - 1;
@@ -39,13 +52,17 @@ public class Set {
             if(current.get(classIndex).equalsIgnoreCase("no"))
                 neg++;
         }
-
         if(pos>neg || pos==neg)
             return "yes";
         else
             return "no";
     }
 
+    /**
+     * Adds the records of a file to a set
+     * @param file the data file to be imported
+     * @throws Exception if cannot read file
+     */
     public void addFile(File file) throws Exception {
         BufferedReader br = new BufferedReader(new FileReader(file));
         Scanner scanner;
@@ -58,24 +75,42 @@ public class Set {
             }
             table.add(tuple);
         }
+        br.close();
     }
 
+    /**
+     * Returns the size of the set
+     * @return int of the size of the set
+     */
     public int getSize()
     {
         return table.size();
     }
 
+    /**
+     * Retrieves String of set based on row x column
+     * @param row the row in the set
+     * @param col the column in the set
+     * @return String with row x column value
+     */
     public String getRowCol(int row, int col)
     {
         ArrayList<String> tuple = table.get(row);
         return tuple.get(col);
     }
 
+    /**
+     * Clears the set
+     */
     public void clear()
     {
         table.clear();
     }
 
+    /**
+     * Determines if a set is empty
+     * @return boolean true or false
+     */
     public boolean isEmpty(){
         if (table.isEmpty())
             return true;
@@ -83,10 +118,21 @@ public class Set {
             return false;
     }
 
+    /**
+     * Gets row of a set
+     * @param index the row number
+     * @return the tuple
+     */
     public ArrayList<String> getRow(int index) {
+
         return table.get(index);
     }
 
+    /**
+     * Gets column of a set
+     * @param index the colum number
+     * @return the values for the column
+     */
     public ArrayList<String> getCol(int index) {
         ArrayList<String> returnTuple = new ArrayList<>();
         for (int i=0; i<table.size(); i++) {
@@ -97,6 +143,10 @@ public class Set {
         return returnTuple;
     }
 
+    /**
+     * Removes a tuple from the set
+     * @param index the row of the tuple to be removed
+     */
     public void removeTuple(int index)
     {
         table.remove(index);
@@ -109,6 +159,5 @@ public class Set {
             }
             System.out.println();
         }
-        //System.out.println();
     }
 }
--- src/Tree.java
+++ src/Tree.java
@@ -1,5 +1,4 @@
 import org.javatuples.Quartet;
-
 import java.util.ArrayList;
 
 /**
@@ -9,11 +8,21 @@ public class Tree {
     private Node root;
     private ArrayList<Attribute> attrList;
 
+    /**
+     * Creates the tree by creating a root node that contains the training Set.
+     * @param trainingSet Set of tuples that will be used as the training set
+     * @param attributeList the attributes that define the tuples in the training set
+     */
     public Tree(Set trainingSet, ArrayList<Attribute> attributeList ){
         this.root=new Node(trainingSet);
         this.attrList = attributeList;
     }
 
+    /**
+     * Partitions the root node by dividing training set and
+     * placing the paritions in children nodes
+     * @param classIndex the row number that contains the tuple label
+     */
     public void partitionTree(int classIndex) {
 
         //gets the best splitting attribute
@@ -23,19 +32,28 @@ public class Tree {
         root.setLabel(splitting.getName());
 
         //for each of the possible values of the splitting attribute
+        //Eg. attribute age has values youth, middle_aged, and senior
         for (Quartet currentQuartet : splitting.getAllowedValues()) {
+
             String value = (String) currentQuartet.getValue0();
             Set subset = new Set();
+
             //check if a tuple has the splitting attribute value
             for (int i = 0; i < root.getPayload().getSize(); i++) {
                 ArrayList<String> tuple = root.getPayload().getRow(i);
                 for (int j = 0; j < tuple.size(); j++) {
                     if (tuple.get(j).equalsIgnoreCase(value)) {
-                        subset.addTuple(tuple); //if it does, move it to a subset
+                        tuple.remove(j);
+                        //if it does, add it to a new subset
+                        subset.addTuple(tuple);
                         break;
                     }
                 }
             }
+
+            //add the subset to a new node
+            //label the node with attribute value
+            //add as child to root node
             Node node = new Node(subset);
             node.setLabel(value);
             root.addChild(node, root);
@@ -43,7 +61,10 @@ public class Tree {
         root.getPayload().clear();
     }
 
-    public void printChildren(){
+    /**
+     * Prints the root and children of the tree
+     */
+    public void printTree(){
         if (root==null)
             return;
         else if (!root.getChildren().isEmpty()){
@@ -55,11 +76,17 @@ public class Tree {
         }
     }
 
+    /**
+     * Given a tupple and it's attribute list, determine its class label
+     * @param tuple the tuple to be tested
+     * @param attributeList the list of attributes of the tuple (must match the attributes from the tree)
+     * @return a String with the label of the tuple
+     */
     public String getClass(ArrayList<String> tuple, ArrayList<Attribute> attributeList) {
         String matchingAttribute;
         for(Attribute attrCurrent: attributeList)
         {
-            //if attribute name matches root's label
+            //if attribute from attribute name matches root's label
             if( (matchingAttribute=attrCurrent.getName()).equalsIgnoreCase(root.getLabel()))
             {
                 //check each child of root
@@ -68,10 +95,10 @@ public class Tree {
                     //for each value in the tuple
                     for (String currentString: tuple)
                     {
-                        //if one matches the label of the child node
+                        //if a children of root has the same label as the tuple
                         if ( currentString.equals(nodeCurrent.getLabel()) )
                         {
-                            //return the class label that appears the most in the node;
+                            //return the class label that appears the most in the set of the child node;
                             return nodeCurrent.getPayload().getClassLabelFrequent();
                         }
                     }
@@ -81,6 +108,14 @@ public class Tree {
         return null;
     }
 
+    /**
+     * Helper method used by partitionTree(int classIndex) to determine the best attribute
+     * partitioning the tree
+     * @param attributeList
+     * @param trainingSet
+     * @param classIndex
+     * @return
+     */
     private Attribute attributeSelection(ArrayList<Attribute> attributeList, Set trainingSet, int classIndex) {
         double lowest = 100;
         int index = -1;
@@ -93,9 +128,9 @@ public class Tree {
                 index = i;
             }
         }
-        //temporary save and remove the best attribute from the attributeList - undone
-        //splitting on an attribute that has yes or no for values doesn't work - all the others do.
-        //it doesn't work because there are to columns with either value.
+        //remove the best attribute from the attributeList - undone
+        //splitting on an attribute that has yes or no for values doesn't work
+        //because class column also has those values
         return attributeList.get(index);
     }
 }
